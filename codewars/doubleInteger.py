71ed645a4935dcf7581986c4a90c51a982503654
Abito Julius <abitojulius@gmail.com>
2026-08-29 11:19:32 +0700

    Create doubleInteger.py

---
+++ codewars/doubleInteger.py
@@ -0,0 +1,11 @@
+"""
+Problem:
+Description:
+Code as fast as you can! You need to double the integer and return it.
+
+Link : https://www.codewars.com/kata/53ee5429ba190077850011d4
+"""
+
+# Solution:
+def double_integer(i):
+    return i * 2
