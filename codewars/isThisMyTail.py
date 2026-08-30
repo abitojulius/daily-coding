d4f28b5c1f1ed5a1c74e2a02cd90b2d721fe6a5b
Abito Julius <abitojulius@gmail.com>
2026-08-30 13:16:21 +0700

    Create isThisMyTail.py

---
+++ codewars/isThisMyTail.py
@@ -0,0 +1,19 @@
+"""
+Problem:
+Description:
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be non empty strings, and normal letters.
+
+Link : https://www.codewars.com/kata/56f695399400f5d9ef000af5
+"""
+
+# Solution:
+def correct_tail(body, tail):
+    sub = body[len(body) - 1]
+    if sub == tail:
+        return True
+    else:
+        return False
